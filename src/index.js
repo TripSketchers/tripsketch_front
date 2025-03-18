cea8c22eb109dd1c5e5129c32c65e567db89ba51
mini3100 <mini3100@naver.com>
2025-03-18 18:30:46 +0900

    Initial commit: 프론트 기초 세팅

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Reset/>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
+  </QueryClientProvider>
+);
+
+reportWebVitals();
