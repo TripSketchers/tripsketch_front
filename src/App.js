ca3b594ac61d8c3696ecf3c8fb225d0731947c40
mini3100 <mini3100@naver.com>
2025-03-20 14:48:34 +0900

    Feat: 라우트 설정 및 기본 UI 구성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Signin from "./pages/Signin";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <Header/>
+      <div>
+        <Routes>
+          <Route path='/' element={ <Main/> }/>
+          <Route path='/signin' element={ <Signin/> }/>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
   );
 }
 
