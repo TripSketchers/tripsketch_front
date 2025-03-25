8bea44158ac619b3060b5b765a283b8eb6b4a4c1
mini3100 <mini3100@naver.com>
2025-03-25 19:38:49 +0900

    Feat: 회원가입 기능 1차 완료(Aop, 중복 검사 미구현)

--- src/components/SigninContainer/SigninContainer.jsx
+++ src/components/SigninContainer/SigninContainer.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 /** @jsxImportSource @emotion/react */
 import * as S from './Style';
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { FaGoogle } from "react-icons/fa";
 import { SiNaver } from "react-icons/si";
 import SigninButton from '../SigninButton/SigninButton';
+import { instance } from '../../api/config/instance';
 
 function SigninContainer({ isSignin, isRightPanelActive }) {
     const text = isSignin?"로그인":"회원가입";
+    const [ errorMessage, setErrorMessage ] = useState("");
+    const [ signupUser, setSignupUser ] = useState({
+        email: "",
+        password: "",
+        checkPassword: ""
+    })
+
+    const handleInputChange = (e) => {
+        setSignupUser({
+            ...signupUser,
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    useEffect(() => {
+        if(signupUser.checkPassword != "" && signupUser.password != signupUser.checkPassword) {
+            setErrorMessage("*비밀번호가 일치하지 않습니다.");
+        } else {
+            setErrorMessage("");
+        }
+    }, [signupUser.password, signupUser.checkPassword])
+
+    const handleSignupSubmit = async (e) => {
+        e.preventDefault(); // 기본 동작 막기
+        const signupData = {
+            email: signupUser.email,
+            password: signupUser.password
+        }
+        try {
+            const response = await instance.post("/auth/signup", signupData);
+        } catch (error) {
+            console.error(error);
+            alert(error.response.data.email);
+        }
+    }
 
     return (
         <div css={S.SLayout(isSignin, isRightPanelActive)}>
@@ -19,10 +55,13 @@ function SigninContainer({ isSignin, isRightPanelActive }) {
                     <a href="#"><SiNaver size={18}/></a>
                 </div>
                 <span>이메일로 {text}</span>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                {isSignin ? <></> : <input type="password" placeholder="Re-enter Password" />}
-                <SigninButton>{text}</SigninButton>
+                <input type="email" name="email" placeholder="Email" onChange={handleInputChange}/>
+                <input type="password" name="password" placeholder="Password" onChange={handleInputChange}/>
+                {isSignin ? <></> : <>
+                                        <input type="password" name="checkPassword" placeholder="Re-enter Password" onChange={handleInputChange}/>
+                                        <div css={S.ErrorMsg}>{errorMessage}</div>
+                                    </>}
+                <SigninButton type="submit" onClick={handleSignupSubmit}>{text}</SigninButton>
             </form>
         </div>
     );
