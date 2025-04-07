673e043c899e5e4fbe51e4b3bfadf5f4e965d8ce
mini3100 <mini3100@naver.com>
2025-04-07 14:56:19 +0900

    Feat: TripRoute 접근 권한 설정

--- src/components/Routes/TripRoute.jsx
+++ src/components/Routes/TripRoute.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import TripAlbum from '../../pages/TripAlbum/TripAlbum';
 import TripPlan from '../../pages/TripPlan/TripPlan';
 import TripAlbumUpload from '../../pages/TripAlbumUpload/TripAlbumUpload';
 import TripCreate from '../../pages/TripCreate/TripCreate';
+import { useQueryClient } from '@tanstack/react-query';
 
 function TripRoute(props) {
+
+    const queryClient = useQueryClient();
+    const principal = queryClient.getQueryState(["getPrincipal"]);
+
+    if (!principal?.data?.data) {
+        return <Navigate to="/" />;
+    }
+
     return (
         <Routes>
             <Route path='create' element={<TripCreate/>}/>
