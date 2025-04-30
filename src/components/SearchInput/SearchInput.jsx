69f45e3348e538b6104918e7f841c0a61154bbec
wjdals717 <bjm3305@naver.com>
2025-04-30 16:17:25 +0900

    Fix: 충돌해결

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import * as S from "./Style";
 import { IoSearch } from "react-icons/io5";
 
-function SearchInput({ placeholder, onSearch }) {
+function SearchInput({ placeholder, onSearch, setIsShow }) {
 	const [searchValue, setSearchValue] = useState(null);
 
 	const handleChange = (e) => {
@@ -29,6 +29,8 @@ function SearchInput({ placeholder, onSearch }) {
 				placeholder={placeholder}
 				onKeyDown={handleKeyDown}
 				onChange={handleChange}
+				onFocus={()=>setIsShow(true)}
+				onBlur={()=>setIsShow(false)}
 			/>
 			<button css={S.SButton} onClick={handleSearchBtn}>
 				<IoSearch />
