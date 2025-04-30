9037f9730a7770e8a7222ba0614b3c376b4971a8
mini3100 <mini3100@naver.com>
2025-04-30 16:44:13 +0900

    Fix: 리팩토링 충돌 해결

--- src/components/AlbumComponents/AlbumDetailModal/AlbumDetailModal.jsx
+++ src/components/AlbumComponents/AlbumDetailModal/AlbumDetailModal.jsx
@@ -8,129 +8,132 @@ import { instance } from "../../../api/config/instance";
 import { useQueryClient } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import ConfirmModal from "../../ConfirmModal/ConfirmModal";
-import ModalLayout from "../../ModalLayout/ModalLayout";
 
 function AlbumDetailModal({ photo, onClose }) {
-	const { tripId } = useParams();
+    const { tripId } = useParams();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [memo, setMemo] = useState(photo.memo); // 메모 상태
+    const [isEditing, setIsEditing] = useState(false); // 편집 모드
 
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [memo, setMemo] = useState(photo.memo); // 메모 상태
-	const [isEditing, setIsEditing] = useState(false); // 편집 모드
+    const handleEditBtn = () => {
+        setIsEditing(true);
+    };
 
-	const handleEditBtn = () => {
-		setIsEditing(true);
-	};
+    const handleMemoChange = (e) => {
+        setMemo(e.target.value);
+    };
 
-	const handleMemoChange = (e) => {
-		setMemo(e.target.value);
-	};
+    const handleDelete = async () => {
+        try {
+            const option = {
+                headers: {
+                    Authorization: localStorage.getItem("accessToken"),
+                },
+            };
+            await instance.delete(
+                `/trips/${tripId}/album/photos/${photo.photoId}`,
+                option
+            );
+            alert(`사진 ${photo.photoId} 삭제 완료`);
+            setIsEditing(false); // 편집 모드 종료
+            onClose();
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-	const handleDelete = async () => {
-		try {
-			const option = {
-				headers: {
-					Authorization: localStorage.getItem("accessToken"),
-				},
-			};
-			await instance.delete(
-				`/trips/${tripId}/album/photos/${photo.photoId}`,
-				option
-			);
-			alert(`사진 ${photo.photoId} 삭제 완료`);
-			setIsEditing(false); // 편집 모드 종료
-			onClose();
-		} catch (error) {
-			console.log(error);
-		}
-	};
+    const handleEditClick = async () => {
+        try {
+            const option = {
+                headers: {
+                    "Content-Type": "text/plain",
+                    Authorization: localStorage.getItem("accessToken"),
+                },
+            };
+            await instance.put(
+                `/trips/${tripId}/album/${photo.photoId}`,
+                memo,
+                option
+            );
+            alert("사진 메모 수정 완료!");
+            setIsEditing(false); // 편집 모드 종료
+        } catch (error) {
+            alert(error.response.data.sendFail);
+        }
+    };
 
-	const handleEditClick = async () => {
-		try {
-			const option = {
-				headers: {
-					"Content-Type": "text/plain",
-					Authorization: localStorage.getItem("accessToken"),
-				},
-			};
-			await instance.put(
-				`/trips/${tripId}/album/${photo.photoId}`,
-				memo,
-				option
-			);
-			alert("사진 메모 수정 완료!");
-			setIsEditing(false); // 편집 모드 종료
-		} catch (error) {
-			alert(error.response.data.sendFail);
-		}
-	};
-
-	return (
-		<ModalLayout onClose={onClose}>
-			<div css={S.closeBtn} onClick={() => onClose}>
-				<button>
-					<IoClose />
-				</button>
-			</div>
-			<div css={S.SPhotoContainer}>
-				<img src={photo.photoUrl} draggable="false" />
-			</div>
-			<div>
-				<div css={S.SPhotoMemo}>
-					{!isEditing ? (
-						<div>{memo}</div>
-					) : (
-						<textarea
-							autoFocus
-							value={memo}
-							onChange={handleMemoChange}
-							placeholder="사진에 대한 추억을 적어보세요!"
-						/>
-					)}
-				</div>
-				<div css={S.SEditBox}>
-					<span>
-						{photo.date} {photo.place}
-					</span>
-					{!isEditing ? (
-						<div>
-							<button
-								css={S.deleteBtn}
-								onClick={() => setIsModalOpen(true)}
-							>
-								<FaTrash />
-							</button>
-							<button css={S.editBtn} onClick={handleEditBtn}>
-								<MdEdit />
-							</button>
-						</div>
-					) : (
-						<div css={S.SEditClickBtn}>
-							<button
-								className="cancel"
-								onClick={() => {
-									setIsEditing(false);
-								}}
-							>
-								취소
-							</button>
-							<button className="edit" onClick={handleEditClick}>
-								수정 완료
-							</button>
-						</div>
-					)}
-				</div>
-			</div>
-			{isModalOpen && (
-				<ConfirmModal
-					title="사진을 삭제하시겠어요?"
-					message="삭제 시 복구할 수 없습니다."
-					confirmText="삭제"
-					onClose={() => setIsModalOpen(false)}
-					onConfirm={handleDelete}
-				/>
-			)}
-		</ModalLayout>
-	);
+    return (
+        <div css={S.backdrop} onClick={() => onClose()} >
+            <div css={S.modal} onClick={(e) => e.stopPropagation()}>
+                <div css={S.closeBtn} onClick={() => onClose()}>
+                    <button>
+                        <IoClose />
+                    </button>
+                </div>
+                <div css={S.SPhotoContainer}>
+                    <img src={photo.photoUrl} draggable="false" />
+                </div>
+                <div>
+                    <div css={S.SPhotoMemo}>
+                        {!isEditing ? (
+                            <div>{memo}</div>
+                        ) : (
+                            <textarea
+                            autoFocus
+                                value={memo}
+                                onChange={handleMemoChange}
+                                placeholder="사진에 대한 추억을 적어보세요!"
+                            />
+                        )}
+                    </div>
+                    <div css={S.SEditBox}>
+                        <span>
+                            {photo.date} {photo.place}
+                        </span>
+                        {!isEditing ? (
+                            <div>
+                                <button
+                                    css={S.deleteBtn}
+                                    onClick={()=>setIsModalOpen(true)}
+                                >
+                                    <FaTrash />
+                                </button>
+                                <button css={S.editBtn} onClick={handleEditBtn}>
+                                    <MdEdit />
+                                </button>
+                            </div>
+                        ) : (
+                            <div css={S.SEditClickBtn}>
+                                <button
+                                    className="cancel"
+                                    onClick={() => {
+                                        setIsEditing(false)
+                                    }}
+                                >
+                                    취소
+                                </button>
+                                <button
+                                    className="edit"
+                                    onClick={handleEditClick}
+                                >
+                                    수정 완료
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                {isModalOpen && (
+                    <ConfirmModal
+                        title="사진을 삭제하시겠어요?"
+                        message="삭제 시 복구할 수 없습니다."
+                        confirmText="삭제"
+                        onClose={() => setIsModalOpen(false)}
+                        onConfirm={handleDelete}
+                    />
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default AlbumDetailModal;
--- src/components/AlbumComponents/AlbumDetailModal/Style.js
+++ src/components/AlbumComponents/AlbumDetailModal/Style.js
@@ -5,6 +5,23 @@ link.href = 'https://fonts.googleapis.com/css2?family=Nanum+Pen+Script&display=s
 link.rel = 'stylesheet';
 document.head.appendChild(link);
 
+export const backdrop = css`
+    position: fixed;
+    top: 0; left: 0; right: 0; bottom: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+`;
+
+export const modal = css`
+    background: #fff;
+    padding: 10px 15px;
+    width: 400px;
+    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+`;
+
 export const closeBtn = css`
     display: flex;
     justify-content: right;
