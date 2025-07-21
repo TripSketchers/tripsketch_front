88a8720ff7a02890c9292d115d12c2eef33aac1a
wjdals717 <bjm3305@naver.com>
2025-07-22 01:12:44 +0900

    Fix: firebase 서비스 계정 변경

--- src/api/Firebase/Firebase.js
+++ src/api/Firebase/Firebase.js
@@ -8,17 +8,18 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAH5d7k4qo7LFPA_HLdzVVrHCljoAxBWDM",
-  authDomain: "tripsketch-66e46.firebaseapp.com",
-  projectId: "tripsketch-66e46",
-  storageBucket: "tripsketch-66e46.firebasestorage.app",
-  messagingSenderId: "48000945568",
-  appId: "1:48000945568:web:259b23e3dd5ad485eae8c5"
+  apiKey: "AIzaSyC2wyURU0Hey0HXT9SAOgyEtDh2h5TKNrc",
+  authDomain: "tripsketch-firebase.firebaseapp.com",
+  projectId: "tripsketch-firebase",
+  storageBucket: "tripsketch-firebase.firebasestorage.app",
+  messagingSenderId: "336452739594",
+  appId: "1:336452739594:web:5ee146485522f77d2135f3"
 };
 
 // Initialize Firebase
 // 이미 초기화된 Firebase 앱이 있는 경우 재사용
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+// const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
