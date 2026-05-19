3bb4b33e0f31e4bb517cf84f81f0b9a05cdb81cf
wjdals <bjm3305@naver.com>
2026-05-19 20:18:18 +0900

    Fix: baseURL 변경 - 재배포로 인한 변경

--- src/api/config/instance.js
+++ src/api/config/instance.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
     // axios 공통 설정
-    baseURL: "https://13-124-221-47.sslip.io/api", //  http://localhost:8080
+    baseURL: "https://3.37.254.211.sslip.io/api", //  http://localhost:8080
 });
